618fcf8e72c61a962bdc9bdf7f5260647a67d04e
kogirika <kogirika12@gmail.com>
2020-09-25 16:46:13 +0900

    rubocop導入

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,54 +1,54 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Item, type: :model do
-  describe 'itemの保存' do
+  describe "itemの保存" do
     before do
       @item = FactoryBot.build(:item)
     end
-    context '保存がうまくいくとき' do
-      it 'すべての値が正しく入力されていれば保存できること' do
+    context "保存がうまくいくとき" do
+      it "すべての値が正しく入力されていれば保存できること" do
         expect(@item).to be_valid
       end
-      it 'priceが抜けていても保存できること' do
+      it "priceが抜けていても保存できること" do
         @item.price = nil
         expect(@item).to be_valid
       end
     end
-    context '保存がうまくいかないとき' do
-      it '品名が空だと保存できないこと' do
-        @item.name = ''
+    context "保存がうまくいかないとき" do
+      it "品名が空だと保存できないこと" do
+        @item.name = ""
         @item.valid?
-        expect(@item.errors.full_messages).to include('品名を入力してください')
+        expect(@item.errors.full_messages).to include("品名を入力してください")
       end
-      it 'カテゴリーが空だと保存できない' do
+      it "カテゴリーが空だと保存できない" do
         @item.category_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('区分を入力してください')
+        expect(@item.errors.full_messages).to include("区分を入力してください")
       end
-      it '在庫数が空だと保存できない' do
-        @item.stock = ''
+      it "在庫数が空だと保存できない" do
+        @item.stock = ""
         @item.valid?
-        expect(@item.errors.full_messages).to include('在庫数は0〜100の間で入力してください')
+        expect(@item.errors.full_messages).to include("在庫数は0〜100の間で入力してください")
       end
-      it '在庫数がマイナスだと保存できない' do
+      it "在庫数がマイナスだと保存できない" do
         @item.stock = -1
         @item.valid?
-        expect(@item.errors.full_messages).to include('在庫数は0〜100の間で入力してください')
+        expect(@item.errors.full_messages).to include("在庫数は0〜100の間で入力してください")
       end
-      it '在庫数が101以上だと保存できない' do
+      it "在庫数が101以上だと保存できない" do
         @item.stock = 101
         @item.valid?
-        expect(@item.errors.full_messages).to include('在庫数は0〜100の間で入力してください')
+        expect(@item.errors.full_messages).to include("在庫数は0〜100の間で入力してください")
       end
-      it '価格がマイナスだと保存できない' do
+      it "価格がマイナスだと保存できない" do
         @item.price = -1
         @item.valid?
-        expect(@item.errors.full_messages).to include('価格を入力する場合は0〜1000,000の間で入力してください')
+        expect(@item.errors.full_messages).to include("価格を入力する場合は0〜1000,000の間で入力してください")
       end
-      it '在庫数が100,001以上だと保存できない' do
-        @item.price = 100001
+      it "在庫数が100,001以上だと保存できない" do
+        @item.price = 100_001
         @item.valid?
-        expect(@item.errors.full_messages).to include('価格を入力する場合は0〜1000,000の間で入力してください')
+        expect(@item.errors.full_messages).to include("価格を入力する場合は0〜1000,000の間で入力してください")
       end
     end
   end
